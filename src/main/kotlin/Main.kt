b10b39557acf3cc086dbb9b9964f6600929a8a4b
C1FR1 <c1fr1.stkcps@gmail.com>
2021-04-24 09:42:37 +0000

    added first render

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,33 @@
 import engine.EnigView
-import engine.OpenGL.EnigWindow
+import engine.OpenGL.*
+import org.joml.Matrix4f
+import org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE
 
 fun main() {
 	val window = EnigWindow("LD48")
     val main = Main(window)
-    main.runLoop()
-	println("hello world")
+	main.runLoop()
 }
 
 public class Main(window: EnigWindow) : EnigView(window) {
-	override fun loop(): Boolean {
-        return false;
-	}
 
+	private val floorVAO : VAO = VAO(-10.0f, -2.5f, 20.0f, 5.0f)
+	private val cam : Matrix4f = window.getSquarePerspectiveMatrix(100f)
+	private val shader = ShaderProgram("textureShader")
+	private val texture = Texture("floor0.png")
+
+	override fun loop() : Boolean {
+		FBO.prepareDefaultRender()
+		floorVAO.prepareRender()
+		shader.enable()
+		texture.bind()
+		shader.setUniform(0, 0, cam)
+		floorVAO.draw()
+		floorVAO.unbind()
+
+		if (window.keys[GLFW_KEY_ESCAPE] > 1) {
+			return true
+		}
+        return false
+	}
 }
